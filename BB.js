09e764b0e2e7f1339937c95195b22a774da1788e
Alexis Marien <alexis.marien1@ge.com>
2018-12-03 20:11:32 -0500

    moved geolocation to branch

--- BB.js
+++ BB.js
@@ -118,33 +118,6 @@ function initMap() {
     });
     var marker = new google.maps.Marker({position: detroit, map: map});
 
-    infoWindow = new google.maps.InfoWindow;
-
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(function(position) {
-            var pos = {
-                lat: position.coords.latitude,
-                lng: position.coords.longitude
-            };
-
-            map.setCenter(pos);
-            marker = new google.maps.Marker({position: pos, map: map});
-
-        }, function() {
-            handleLocationError(true, infoWindow, map.getCenter());
-        });
-    } else {
-        // Browser doesn't support Geolocation
-        handleLocationError(false, infoWindow, map.getCenter());
-    }
-    function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-        infoWindow.setPosition(pos);
-        infoWindow.setContent(browserHasGeolocation ?
-            'Error: The Geolocation service failed.' :
-            'Error: Your browser doesn\'t support geolocation.');
-        infoWindow.open(map);
-    }
-
 }
 
 function placeTrucksOnMap(trucks) {
@@ -252,10 +225,10 @@ function mapTruckLocations(list) {
     var trucks = new Array();
 
     for (var i = 0; i < list.length; i++) {
-        trucks.push(new Array(list[i].name,
+        trucks.push(list[i].name,
             list[i].device.gpsLat,
             list[i].device.gpsLon,
-            i + 1))
+            i + 1)
     }
 
     placeTrucksOnMap(trucks);
